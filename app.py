f9178c798d401f299c331e1ce9b2018c9adc09a7
Nilakantha Sahu <nilakanthasahu496@gmail.com>
2026-05-07 13:54:34 +0530

    chore: setup Vercel configuration and include database

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-# Use a local SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
+# Use a local SQLite database that is safe for serverless read-only
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'portfolio.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
